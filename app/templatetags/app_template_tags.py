d06fd4aa2513f92ce6be0f6e5cec201323900fc1
Yushan Huang <2488116h@student.gla.ac.uk>
2020-03-31 20:18:19 +0100

    updated

--- app/templatetags/app_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from app.models import WhiskyList
-
-register = template.Library()
-
-@register.inclusion_tag('app/categories.html')
-def get_category_list(current_category=None):
-    return {'categories': WhiskyList.objects.all(),
-            'current_category': current_category}
